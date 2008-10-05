e5f4e0f4755134b584edeee500abe23475b0eb94
jezell <jezell@gmail.com>
2008-10-05 02:08:38 -0500

    Move collection and threading classes to their own namespace

---
+++ IServiceOriented.ServiceBus/Collections/ReadOnlyDictionary.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using IServiceOriented.ServiceBus.Threading;
+
+namespace IServiceOriented.ServiceBus.Collections
+{
+    /// <summary>
+    /// Represents a read only collection of keys and values.
+    /// </summary>
+    /// <typeparam name="K">Key type</typeparam>
+    /// <typeparam name="V">Value type</typeparam>
+    [Serializable]
+    [CollectionDataContract]
+    public class ReadOnlyDictionary<K, V> : IReadOnlyDictionary<K, V>
+    {
+        IDictionary<K, V> _dictionary;
+
+        public ReadOnlyDictionary()
+            : this(new Dictionary<K,V>(), false)
+        {
+            
+        }
+
+        public ReadOnlyDictionary(IEnumerable<KeyValuePair<K, V>> pairs)
+        {
+            _dictionary = new Dictionary<K, V>();
+            foreach (KeyValuePair<K, V> p in pairs)
+            {
+                _dictionary.Add(p);
+            }
+        }
+
+        public ReadOnlyDictionary(IDictionary<K, V> from)
+            : this(from, false)
+        {
+        }
+
+        private void Add(KeyValuePair<K,V> pair)
+        {
+            _dictionary.Add(pair.Key, pair.Value);
+        }
+
+        public ReadOnlyDictionary(IDictionary<K, V> from, bool copy)
+        {
+            if (copy)
+            {
+                _dictionary = new Dictionary<K, V>(from);
+            }
+            else
+            {
+                _dictionary = from;
+            }
+        }
+
+        #region IReadOnlyDictionary<K,V> Members
+
+        public IEnumerable<K> Keys
+        {
+            get { return _dictionary.Keys; }
+        }
+
+        public IEnumerable<V> Values
+        {
+            get { return _dictionary.Values; }
+        }
+
+        public V this[K key]
+        {
+            get { return _dictionary[key]; }
+        }
+
+        public int Count
+        {
+            get { return _dictionary.Count; }
+        }
+
+        public bool ContainsKey(K key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public bool Contains(KeyValuePair<K, V> value)
+        {
+            return _dictionary.Contains(value);
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            return _dictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _dictionary.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
---
+++ IServiceOriented.ServiceBus/Threading/ILockedObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+
+namespace IServiceOriented.ServiceBus.Threading
+{
+    /// <summary>
+    /// Gaurds an object with locks.
+    /// </summary>
+    /// <typeparam name="TRead">Read only type of the contained data.</typeparam>
+    /// <typeparam name="TWrite">Writeable type of the contained data.</typeparam>
+    public interface ILockedObject<TRead, TWrite> : IDisposable
+    {
+        /// <summary>
+        /// Access the contained value for read access.
+        /// </summary>
+        /// <param name="action"></param>
+        void Read(Action<TRead> action);
+        /// <summary>
+        /// Access the contained value for write access.
+        /// </summary>
+        /// <param name="action"></param>
+        void Write(Action<TWrite> action);
+        
+        /// <summary>
+        /// Access the contained value with write access and set to a new value.
+        /// </summary>
+        /// <param name="value"></param>
+        void SetValue(TWrite value);
+            
+    }
+    
+    
+    
+         
+
+}
